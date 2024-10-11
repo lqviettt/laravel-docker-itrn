05d87b3559c4207280fc9170abcb8f181204c00e
vietlq3104 <vietlq3104@cowell.vn>
2024-10-11 14:14:24 +0700

    add api search, filter orderController

--- example-app/app/Http/Controllers/OrderController.php
+++ example-app/app/Http/Controllers/OrderController.php
@@ -17,26 +17,48 @@ class OrderController extends Controller
      *
      * @return JsonResponse
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        $order = Order::with(['orderItem.product'])->get()->map(function ($order) {
-            return [
-                'id' => $order->id,
-                'customer_name' => $order->customer_name,
-                'customer_phone' => $order->customer_phone,
-                'status' => $order->status,
-                'shipping_address' => $order->shipping_address,
-                'order_item' => $order->orderItem->map(function ($item) {
-                    return [
-                        'id' => $item->id,
-                        'order_id' => $item->order_id,
-                        'product_name' => $item->product->name,
-                        'quantity' => $item->quantity,
-                        'price' => $item->price,
-                    ];
-                }),
-            ];
-        });
+        $status = $request->input('status');
+        $search = $request->input('search');
+        $productName = $request->input('productName');
+
+        $order = Order::query()
+            ->when($search, function ($query) use ($search) {
+                $query->where(function ($query) use ($search) {
+                    $query->where('customer_name', 'like', $search . '%')
+                        ->orWhere('customer_phone', 'like', $search . '%')
+                        ->orWhere('shipping_address', 'like', '%' . $search . '%');
+                });
+            })
+            ->when($status, function ($query) use ($status) {
+                return $query->where('status', $status);
+            })
+            ->when($productName, function ($query) use ($productName) {
+                return $query->whereHas('orderItem.product', function ($query) use ($productName) {
+                    $query->where('name', 'like', '%' . $productName . '%');
+                });
+            })
+            ->with('orderItem.product')
+            ->get()
+            ->map(function ($order) {
+                return [
+                    'id' => $order->id,
+                    'customer_name' => $order->customer_name,
+                    'customer_phone' => $order->customer_phone,
+                    'status' => $order->status,
+                    'shipping_address' => $order->shipping_address,
+                    'order_item' => $order->orderItem->map(function ($item) {
+                        return [
+                            'id' => $item->id,
+                            'order_id' => $item->order_id,
+                            'product_name' => $item->product->name,
+                            'quantity' => $item->quantity,
+                            'price' => $item->price,
+                        ];
+                    }),
+                ];
+            });
 
         return response()->json($order);
     }
