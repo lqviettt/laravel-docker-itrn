c3da457ac17ac049287ea87dfa71dc98b4aca865
vietlq3104 <vietlq3104@cowell.vn>
2024-10-28 11:46:53 +0700

    update send-mail

--- example-app/resources/views/emails/order-success.blade.php
+++ example-app/resources/views/emails/order-success.blade.php
@@ -8,18 +8,8 @@
 <body>
     <h1>Đơn hàng của bạn đã được tạo!</h1>
     <p><strong>Tên khách hàng:</strong> {{ $order->lastname . ' ' . $order->firstname }}</p>
-    <p><strong>Số điện thoại:</strong> {{ $order['customer_phone'] }}</p>
-    <p><strong>Địa chỉ giao hàng:</strong> {{ $order['shipping_address'] }}</p>
-    <h2>Chi tiết đơn hàng:</h2>
-    <ul>
-        @foreach ($order['order_items'] as $item)
-        <li>
-            Sản phẩm ID: {{ $item['product_id'] }},
-            Số lượng: {{ $item['quantity'] }},
-            Giá: {{ $item['price'] }} VND
-        </li>
-        @endforeach
-    </ul>
+    <p><strong>Số điện thoại:</strong> {{ $order->customer_phone }}</p>
+    <p><strong>Địa chỉ giao hàng:</strong> {{ $order->shipping_address }}</p>
+    <p><strong>Địa chỉ giao hàng:</strong> {{ $order->status }}</p>
 </body>
-
 </html>
