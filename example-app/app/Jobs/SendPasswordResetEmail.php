1f0c3c9877cbb7a6e412d2e1ef3b6e0ec7da6235
QuocViet <lqviettt3403@gmail.com>
2024-11-28 14:13:07 +0700

    fogotpassword

---
+++ example-app/app/Jobs/SendPasswordResetEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendPasswordResetEmail implements ShouldQueue
+{
+    use Dispatchable, Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to('quocviettt45@gmail.com')->send(new \App\Mail\PasswordResetMail($this->user));
+    }
+}
